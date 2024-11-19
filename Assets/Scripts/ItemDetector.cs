111f4a1b5887311533d22da1be25d6977d3a60b3
suyeon0407 <163252808+suyeon0407@users.noreply.github.com>
2024-11-19 11:14:08 +0900

    스크립트 수정

--- Assets/Scripts/ItemDetector.cs
+++ Assets/Scripts/ItemDetector.cs
@@ -44,4 +44,33 @@ public class ItemDetector : MonoBehaviour
         Grizmos.color = Color.blue;
         Grizmos.DrawSphere(transform.position, checkRadius);
     }
+    private void CheckForItems()
+{
+    Colider[] hitColiders = Physics.OverlapSphere(transform.position, checkRadius);
+
+    float closestDistance = float.MaxValue;
+    CollectibleItem closestItem = null;
+
+    foreach (Collider collider in hitColiders)
+    {
+        CollectibleItem item = collider.GetCompoent<CollectibleItem>();
+        if (item !=null && item.canCollect)
+        {
+            float distance = Vector3.Distance(transform.Position, item.transform.position);
+            if (distance<closestDistance)
+            {
+                closestDistance = distance;
+                closestItem = item;
+            }
+        }
+    }
+    if(closestItem !=currentNearbyItem)
+    {
+        currentNearbyItem = closestItem;
+        if(currentNearbyItem !=null)
+        {
+            Debug.Log($"[E] 키를 눌러 {currentNearbyItem.itemName} 수집");
+        }
+    }
+}
 }
